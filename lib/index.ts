1f0a4e4d55d01d94ae0ce9b1a85ce3e8d7d98e2c
Manu De Buck <manu.debuck@telenet.be>
2020-08-28 12:17:41 +0200

    Simplify index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -1,17 +1,2 @@
 export * from "./models/Profile";
-export {
-  ClientConfig,
-  Client,
-  createJournyClient,
-  JourneyClientError,
-  Properties,
-  ClientResponse,
-  ClientResponseData,
-  ApiKeySpecs,
-  TrackEventArguments,
-  TrackPropertiesArguments,
-  GetProfileArguments,
-  ProfileResponse,
-  GetTrackingSnippetArguments,
-  TrackingSnippetResponse,
-} from "./Client";
+export * from "./Client";
